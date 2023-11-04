ea557365b81417b180d356bd2636750285c62903
rob seward <stuff@robs-MacBook-Pro.local>
2023-11-04 16:49:38 +0000

    sat closing time change

--- src/components/sections/OpeningTimes.jsx
+++ src/components/sections/OpeningTimes.jsx
@@ -36,11 +36,11 @@ export function OpeningTimes() {
                     <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">31st Oct</dd>
                   </div>
                   <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-                    <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">9am - 7pm</dt>
+                    <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">9am - 6pm</dt>
                     <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">1st 2nd 3rd 4th Nov</dd>
                   </div>
                   <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-                    <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500"> 9am - 6pm</dt>
+                    <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500"> 10am - 4pm</dt>
                     <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">5th Nov</dd>
                   </div>
                 </dl>
