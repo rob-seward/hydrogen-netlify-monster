8280932d19c833ef3c8baf6044ba2afd01eab62e
rob seward <stuff@robs-MacBook-Pro.local>
2023-12-12 17:15:50 +0000

    internet order status update

--- src/components/sections/HomeSplitBanner.jsx
+++ src/components/sections/HomeSplitBanner.jsx
@@ -35,7 +35,7 @@ export function HomeSplitBanner() {
 </span>
           
           <p className="mt-3 text-lg text-gray-300">
-            Internet Orders Now Closed
+            Internet Orders Openning Shortly
           </p>
           <div className="mt-8">
             <div className="inline-flex rounded-md shadow">
