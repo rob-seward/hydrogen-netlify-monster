7456c6e02dbd7cc171dd6c457276d5dc1b0a9793
rob seward <stuff@robs-MacBook-Pro.local>
2023-12-28 16:21:43 +0000

    collectin dates

--- src/components/sections/HomeSplitBanner.jsx
+++ src/components/sections/HomeSplitBanner.jsx
@@ -35,7 +35,7 @@ export function HomeSplitBanner() {
 </span>
           
           <p className="mt-3 text-lg text-gray-300">
-            Collect from December 28th
+            Collect from shop
           </p>
           <div className="mt-8">
             <div className="inline-flex rounded-md shadow">
