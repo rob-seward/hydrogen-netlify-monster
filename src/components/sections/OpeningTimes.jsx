1e942a4d2bb551e5060358fbe70fda4b8fd89faa
rob seward <stuff@robs-MacBook-Pro.local>
2024-10-26 17:14:57 +0100

    fix time error

--- src/components/sections/OpeningTimes.jsx
+++ src/components/sections/OpeningTimes.jsx
@@ -34,12 +34,12 @@ export function OpeningTimes() {
                     <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">29th Oct</dd>
                   </div>
                   <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-                    <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">Wed Closed</dt>
+                    <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">Wed 10am - 5pm</dt>
                     <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">30th Oct</dd>
                   </div>
                   <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
                   <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">Thur 10am - 5pm</dt>
-                    <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">31stth Oct</dd>
+                    <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">31st Oct</dd>
                   </div>
 
                   <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
