48a2704e5c59fe524743cf571f505798447a44a9
rob seward <stuff@robs-MacBook-Pro.local>
2024-12-16 15:49:19 +0000

    admin new year updates

--- src/components/sections/SingleFuse.jsx
+++ src/components/sections/SingleFuse.jsx
@@ -3,7 +3,7 @@ export function SingleFuse() {
       <div className="bg-gray-900">
         <div className="mx-auto max-w-7xl pt-6  px-4 text-center sm:px-6 lg:py-16 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            <span className="block">Perfect for Bonfire 2024</span>
+            <span className="block">Perfect for New Years 2024</span>
             
           </h1>
           
