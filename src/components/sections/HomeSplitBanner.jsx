cf12e596c27f7d29256b45c15b53fd5c605412c1
rob seward <stuff@robs-MacBook-Pro.local>
2025-10-09 16:49:25 +0100

    new open times

--- src/components/sections/HomeSplitBanner.jsx
+++ src/components/sections/HomeSplitBanner.jsx
@@ -21,7 +21,7 @@ export function HomeSplitBanner() {
 
 <span className="flex">
 <span className="flex-none">
-<p className=" mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">ORDER LINE CLOSED </p>
+<p className=" mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">ORDER ONLINE NOW </p>
 </span>
 
 <span className="flex-none">
@@ -35,7 +35,7 @@ export function HomeSplitBanner() {
 </span>
           
           <p className="mt-3 text-lg text-gray-300">
-            Opens November 2025 for Bonfire
+            Collect on Sat 25th Oct or from 31st Oct - 8th Novmber
           </p>
           
           <div className="mt-8">
--- src/components/sections/OpeningTimes.jsx
+++ src/components/sections/OpeningTimes.jsx
@@ -7,7 +7,7 @@ export function OpeningTimes() {
               Opening Times
             </h2>
             <p className="mt-3 text-xl text-gray-500 sm:mt-4">
-            Order your Bonfire fireworks now and collect from the Saturday 25th Oct - 5th November
+            Order your Bonfire fireworks now and collect from Sat 25th Oct of 31st Oct - 8th November
             </p>
           </div>
         </div>
@@ -26,12 +26,12 @@ export function OpeningTimes() {
                     <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">26th Oct</dd>
                   </div>
                   <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                  <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">Mon - Tues closed</dt>
-                    <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">27th & 28th Oct</dd>
+                  <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">Mon - Thurs CLOSED</dt>
+                    <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">27th & 30th Oct</dd>
                   </div>
                   <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
-                    <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">Wed - Thurs 10am - 5pm</dt>
-                    <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">29th & 30th Oct</dd>
+                    <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500"></dt>
+                    <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">/dd>
                   </div>
                   <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
                     <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">Fri - Sat 10am - 6pm</dt>
@@ -58,7 +58,7 @@ export function OpeningTimes() {
                     <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">7th & 8th Nov </dd>
                   </div>
                   <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-                    <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">Sun 11am - 3pm</dt>
+                    <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">Sun 11am - 2pm</dt>
                     <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">9th Nov</dd>
                   </div>
                   <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
